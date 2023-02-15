16664303cf479223cbf947dace68027f4d57ec84
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-15 23:54:12 +0530

    frontend jenkins

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = merge(
-local.common_tags,
-{Name = "${var.env}-vpc" }
-)
+    local.common_tags,
+    { Name = "${var.env}-vpc" }
+  )
 }
 
 resource "aws_subnet" "main" {
-  count = length(var.subnets_cidr)
-  vpc_id = aws_vpc.main.id
+  count      = length(var.subnets_cidr)
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-subnet" }
+    { Name = "${var.env}-subnet" }
   )
 }
