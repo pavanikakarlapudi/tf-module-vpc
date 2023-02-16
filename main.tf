8e488fc8c52404073cd88028c4dd24b4478c0bbe
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-17 00:48:01 +0530

    frontend jenkins

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ resource "aws_route_table" "public" {
   )
 }
 resource "aws_route_table_association" "public-rt-assoc" {
+  count = length(aws_subnet.public)
   subnet_id      = aws_subnet.public.*.id[count.index]
   route_table_id = aws_route_table.public.id
 }
