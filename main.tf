65cac6ebe3955d72b8b40b309b92cfd4ed371bf4
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-18 23:13:59 +0530

    frontend jenkins

--- main.tf
+++ main.tf
@@ -32,6 +32,25 @@ resource "aws_internet_gateway" "gw" {
   )
 }
 
+resource "aws_eip" "ngw-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id     = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-ngw" }
+  )
+
+}
+
+
+
+
+
 
 
 
