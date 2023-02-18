6a3aece68793ae4b847c193ff1274ad158e87972
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-18 22:41:11 +0530

    frontend jenkins

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
-    env = var.env
-    project = "roboshop"
+    env           = var.env
+    project       = "roboshop"
     business_unit = "ecommerce"
-    owner = "ecommerce-robot"
+    owner         = "ecommerce-robot"
   }
 }
